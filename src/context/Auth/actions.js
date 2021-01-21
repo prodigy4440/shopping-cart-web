f673ab6abf17c3f856960dd86fd5296b35f3a219
Isau AZEEZ <prodigy4442@gmail.com>
2021-01-21 23:48:05 +0100

    clean up and dockerization

--- src/context/Auth/actions.js
+++ src/context/Auth/actions.js
@@ -1,24 +1,25 @@
-import { toast } from 'react-toastify'
+import {toast} from 'react-toastify'
 
-const URL = 'https://oja-oba-service.herokuapp.com'
+// const API_URL = 'https://oja-oba-service.herokuapp.com'
+const {REACT_APP_API_URL} = process.env;
 
 export const loginUser = async (dispatch, loginPayload) => {
     const options = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(loginPayload),
     }
 
     try {
-        dispatch({type:'REQUEST_LOGIN'})
-        const request = await fetch(`${URL}/v1/user/login`, options)
+        dispatch({type: 'REQUEST_LOGIN'})
+        const request = await fetch(`${REACT_APP_API_URL}/v1/user/login`, options)
         const response = await request.json()
-        if(response.status === "SUCCESS"){
-            dispatch({type:'LOGIN_SUCCESS', payload:response.data})
+        if (response.status === "SUCCESS") {
+            dispatch({type: 'LOGIN_SUCCESS', payload: response.data})
             localStorage.setItem('token', JSON.stringify(response.data));
             return response
-        }else {
-            dispatch({type:'LOGIN_ERROR', payload:response.description})
+        } else {
+            dispatch({type: 'LOGIN_ERROR', payload: response.description})
             return response;
         }
 
@@ -28,34 +29,34 @@ export const loginUser = async (dispatch, loginPayload) => {
 
     } catch (error) {
 
-        dispatch({type:'LOGIN_ERROR', payload:error.message})
+        dispatch({type: 'LOGIN_ERROR', payload: error.message})
     }
 }
 export const registerUser = async (dispatch, loginPayload) => {
     const options = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({...loginPayload, email:loginPayload.username}),
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({...loginPayload, email: loginPayload.username}),
     }
 
     try {
-        dispatch({type:'REQUEST_LOGIN'})
-        const request = await fetch(`${URL}/v1/user`, options)
+        dispatch({type: 'REQUEST_LOGIN'})
+        const request = await fetch(`${REACT_APP_API_URL}/v1/user`, options)
         const response = await request.json()
-        if(response.status === "SUCCESS"){
-            dispatch({type:'LOGIN_SUCCESS', payload:response.data})
+        if (response.status === "SUCCESS") {
+            dispatch({type: 'LOGIN_SUCCESS', payload: response.data})
             return response
-        }else {
-            dispatch({type:'LOGIN_ERROR', payload:response.description})
+        } else {
+            dispatch({type: 'LOGIN_ERROR', payload: response.description})
             return response;
         }
     } catch (error) {
 
-        dispatch({type:'LOGIN_ERROR', payload:error.message})
+        dispatch({type: 'LOGIN_ERROR', payload: error.message})
     }
 }
 
 export const logout = (dispatch) => {
-    dispatch({type:'LOGOUT'})
+    dispatch({type: 'LOGOUT'})
     localStorage.removeItem('token');
 }
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,14 +1,17 @@
-import React, { useState, useEffect, createContext } from 'react'
-import { toast } from 'react-toastify';
+import React, {useState, useEffect, createContext} from 'react'
+import {toast} from 'react-toastify';
 import MOCK_DATA from '../MOCK_DATA.json'
-import { isSafari } from '../utils';
+import {isSafari} from '../utils';
+
 export const StateContext = createContext();
+
+const {REACT_APP_API_URL} = process.env;
 
 const Context = (props) => {
     const [open, setOpen] = useState(false);
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [isLogModalOpen, setIsLogModalOpen] = useState(false);
-    const [isAuthenticated, setIsAuthenticated] =useState(false)
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
     const [cartData, setCartData] = useState([]);
     const [modalData, setModalData] = useState(false);
     const [data, setData] = useState({
@@ -23,65 +26,63 @@ const Context = (props) => {
 
     useEffect(() => {
         var iPageTabID = sessionStorage.getItem("tabID");
-      // if it is the first time that this page is loaded
-    if (iPageTabID == null)
-        {
-        var iLocalTabID = localStorage.getItem("tabID");
-          // if tabID is not yet defined in localStorage it is initialized to 1
-          // else tabId counter is increment by 1
-        var iPageTabID = (iLocalTabID == null) ? 1 : Number(iLocalTabID) + 1;
-          // new computed value are saved in localStorage and in sessionStorage
-        localStorage.setItem("tabID",iPageTabID);
-        sessionStorage.setItem("tabID",iPageTabID);
+        // if it is the first time that this page is loaded
+        if (iPageTabID == null) {
+            var iLocalTabID = localStorage.getItem("tabID");
+            // if tabID is not yet defined in localStorage it is initialized to 1
+            // else tabId counter is increment by 1
+            var iPageTabID = (iLocalTabID == null) ? 1 : Number(iLocalTabID) + 1;
+            // new computed value are saved in localStorage and in sessionStorage
+            localStorage.setItem("tabID", iPageTabID);
+            sessionStorage.setItem("tabID", iPageTabID);
         }
     }, [])
 
     //pop modal in other tabs if users do any action to remove or add item
     useEffect(() => {
-      const handleChangeDataLogBroadcast = () => {
-        const channel = new BroadcastChannel("my-channel");
-        changeDataLog !== "" && channel.postMessage({changeDataLog});
+        const handleChangeDataLogBroadcast = () => {
+            const channel = new BroadcastChannel("my-channel");
+            changeDataLog !== "" && channel.postMessage({changeDataLog});
 
-        channel.addEventListener("message", e => {
-             if(localStorage.getItem('openedTab') !== sessionStorage.getItem('tabID')){
-                setChangeDataLog(e.data.changeDataLog)
-             setIsLogModalOpen(true)
-             }
-        });
-      }
-      !isSafari && handleChangeDataLogBroadcast()
+            channel.addEventListener("message", e => {
+                if (localStorage.getItem('openedTab') !== sessionStorage.getItem('tabID')) {
+                    setChangeDataLog(e.data.changeDataLog)
+                    setIsLogModalOpen(true)
+                }
+            });
+        }
+        !isSafari && handleChangeDataLogBroadcast()
     }, [changeDataLog])
 
     //pop modal on load to show users last action
     useEffect(() => {
-     let lastActionData = localStorage.getItem('actionLog')
-     if(localStorage.getItem('openedTab') !== sessionStorage.getItem('tabID')){
-        lastActionData && setChangeDataLog(lastActionData)
-        changeDataLog !== '' && setIsLogModalOpen(true)
-     }
+        let lastActionData = localStorage.getItem('actionLog')
+        if (localStorage.getItem('openedTab') !== sessionStorage.getItem('tabID')) {
+            lastActionData && setChangeDataLog(lastActionData)
+            changeDataLog !== '' && setIsLogModalOpen(true)
+        }
     }, [])
 
 
-        const handleFetchData = async () => {
-            setData({ ...data, loading: true, error: null, result: [] })
-            try {
-                const request = await fetch('https://oja-oba-service.herokuapp.com/v1/product/find?page=0&size=20', {
-                    headers: {
-                        "Authorization" : `Bearer ${JSON.parse(localStorage.getItem('token'))}`
-                    }
-                })
-                const response = await request.json()
-if(response && response.status === "SUCCESS"){
-    const newQuantities = response.data.map(item => ({ ...item, quantitySelected: 0 }))
-    setData({ ...data, loading: false, error: null, result: newQuantities })
-}else {
-    setData({ ...data, loading: false, error: response.description, result: [] })
-}
-            } catch (error) {
-                setData({ ...data, loading: false, error: error.message, result: [] })
+    const handleFetchData = async () => {
+        setData({...data, loading: true, error: null, result: []})
+        try {
+            const request = await fetch(`${REACT_APP_API_URL}/v1/product/find?page=0&size=20`, {
+                headers: {
+                    "Authorization": `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+                }
+            })
+            const response = await request.json()
+            if (response && response.status === "SUCCESS") {
+                const newQuantities = response.data.map(item => ({...item, quantitySelected: 0}))
+                setData({...data, loading: false, error: null, result: newQuantities})
+            } else {
+                setData({...data, loading: false, error: response.description, result: []})
             }
+        } catch (error) {
+            setData({...data, loading: false, error: error.message, result: []})
         }
-
+    }
 
 
     //open modal for item
@@ -118,139 +119,141 @@ if(response && response.status === "SUCCESS"){
      */
     const handleUpdateQuantity = (id, operation) => {
         //get the item
-        const { result } = data;
+        const {result} = data;
         let newItem
-     let newData =   result.map(item => {
-            const { quantitySelected, quantity } = item;
+        let newData = result.map(item => {
+            const {quantitySelected, quantity} = item;
 
-            if(item.id !== id){
+            if (item.id !== id) {
                 return item
-            }if(operation === "add" && (quantitySelected !== quantity || quantity !== 0)){
+            }
+            if (operation === "add" && (quantitySelected !== quantity || quantity !== 0)) {
 
-                newItem = { ...item, quantitySelected: quantitySelected + 1, quantity: quantity - 1 }
+                newItem = {...item, quantitySelected: quantitySelected + 1, quantity: quantity - 1}
                 return newItem
-            }if(operation === 'subtract' && quantitySelected > 0){
-                newItem = { ...item, quantitySelected: quantitySelected - 1, quantity: quantity + 1 }
+            }
+            if (operation === 'subtract' && quantitySelected > 0) {
+                newItem = {...item, quantitySelected: quantitySelected - 1, quantity: quantity + 1}
                 return newItem
-            }else {
+            } else {
                 return item
             }
         })
         setModalData({...newItem})
-        setData({ ...data, result: newData })
+        setData({...data, result: newData})
         //return the item and update the state
     }
 
 
-    const handleSetDataLog = (operator, item ) => {
+    const handleSetDataLog = (operator, item) => {
         localStorage.setItem('openedTab', sessionStorage.getItem("tabID"))
-if(operator === 'add'){
-    console.log(item)
-    setChangeDataLog(`${item} was added to the cart`)
-    localStorage.setItem('actionLog',`${item} was added to the cart` )
-}else if(operator === 'subtract'){
-    setChangeDataLog(`${item} was removed from the cart`)
-    localStorage.setItem('actionLog',`${item} was removed from the cart`)
-}
-    }
-
-const handleAddToCart = (item) => {
-    let updatedItem;
-    let itemIndex = cartData.findIndex(product => product.id === item.id)
-    //calculate the total price and to the data object
-    if(cartData.length === 0 || itemIndex === -1 ){
-        updatedItem = {...item, subTotal:(item.quantitySelected * item.price).toFixed(2) }
-        delete updatedItem.quantity
-        setCartData([...cartData, updatedItem])
-        handleSetDataLog('add', item.name )
-        // setChangeDataLog(`${item.product} was added to the cart`)
-    }else {
-        let newCartData = [...cartData]
-        let foundItem =  newCartData[itemIndex]
-        foundItem.quantitySelected = foundItem.quantitySelected + item.quantitySelected;
-        foundItem.subTotal = ((foundItem.quantitySelected + item.quantitySelected) * item.price).toFixed(2)
-        delete foundItem.quantity
-        setCartData([...newCartData])
-        handleSetDataLog('add', item.name )
-        // setChangeDataLog(`${item.product} was added to the cart`)
-    }
-    // reset the quantity in the main data to 0
-    handleResetQuantity(item)
-    //close the modal
-    handleClose()
-}
-
-const handleResetQuantity = (item) => {
-    let updatedData = data.result.map(product => {
-        if(product.id !== item.id){
-            return product
-        }else {
-            return {...product, quantitySelected:0}
+        if (operator === 'add') {
+            console.log(item)
+            setChangeDataLog(`${item} was added to the cart`)
+            localStorage.setItem('actionLog', `${item} was added to the cart`)
+        } else if (operator === 'subtract') {
+            setChangeDataLog(`${item} was removed from the cart`)
+            localStorage.setItem('actionLog', `${item} was removed from the cart`)
         }
-    })
-
-    setData({...data, result:updatedData})
-}
-
-const handleRemoveItemFromCart = (item) => {
-let deletedItem = cartData.filter(product => item.id === product.id)
-let quantityToAddBackToStock = deletedItem[0].quantitySelected
-let filteredCart = cartData.filter(product => item.id !== product.id)
-handleRestockDeletedItem(item,quantityToAddBackToStock)
-
-setCartData(filteredCart)
-handleSetDataLog('subtract', deletedItem[0].product )
-// setChangeDataLog(`${deletedItem[0].product} was removed from the cart`)
-}
-
-const handleRestockDeletedItem = (item,quantityToAddBackToStock) => {
-    let newUpdatedData = data.result.map(product => {
-        if(product.id !== item.id){
-            return product
-        }else {
-            return {...product, quantity:product.quantity + quantityToAddBackToStock}
-        }
-    })
-    setData({...data, result:newUpdatedData})
     }
 
-    const handleCheckout = async() => {
-       const checkoutData = {
-           requestId: new Date().getTime().toString(16),
-           items: cartData
-       }
-       setIsCheckoutLoading(true)
+    const handleAddToCart = (item) => {
+        let updatedItem;
+        let itemIndex = cartData.findIndex(product => product.id === item.id)
+        //calculate the total price and to the data object
+        if (cartData.length === 0 || itemIndex === -1) {
+            updatedItem = {...item, subTotal: (item.quantitySelected * item.price).toFixed(2)}
+            delete updatedItem.quantity
+            setCartData([...cartData, updatedItem])
+            handleSetDataLog('add', item.name)
+            // setChangeDataLog(`${item.product} was added to the cart`)
+        } else {
+            let newCartData = [...cartData]
+            let foundItem = newCartData[itemIndex]
+            foundItem.quantitySelected = foundItem.quantitySelected + item.quantitySelected;
+            foundItem.subTotal = ((foundItem.quantitySelected + item.quantitySelected) * item.price).toFixed(2)
+            delete foundItem.quantity
+            setCartData([...newCartData])
+            handleSetDataLog('add', item.name)
+            // setChangeDataLog(`${item.product} was added to the cart`)
+        }
+        // reset the quantity in the main data to 0
+        handleResetQuantity(item)
+        //close the modal
+        handleClose()
+    }
 
-       try {
-        const request = await fetch('https://oja-oba-service.herokuapp.com/v1/order/create', {
-            method:"POST",
-            headers: {
-                "Authorization" : `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
-                "Content-Type": 'application/json'
-            },
-            body: JSON.stringify(checkoutData)
-            // body: JSON.stringify(checkoutData)
+    const handleResetQuantity = (item) => {
+        let updatedData = data.result.map(product => {
+            if (product.id !== item.id) {
+                return product
+            } else {
+                return {...product, quantitySelected: 0}
+            }
         })
-        const response = await request.json()
-if(response && response.status === "SUCCESS"){
-    toast.success("Checkout is Successful", {
-        position: toast.POSITION.TOP_CENTER
-      });
-      setCartData([])
-          setIsCheckoutLoading(false)
-}else {
-    console.log(response)
-    toast.error(response.description || response.message, {
-        position: toast.POSITION.TOP_CENTER
-      });
-      setIsCheckoutLoading(false)
-}
-    } catch (error) {
-        toast.error(error.message, {
-            position: toast.POSITION.TOP_CENTER
-          });
-          setIsCheckoutLoading(false)
+
+        setData({...data, result: updatedData})
     }
+
+    const handleRemoveItemFromCart = (item) => {
+        let deletedItem = cartData.filter(product => item.id === product.id)
+        let quantityToAddBackToStock = deletedItem[0].quantitySelected
+        let filteredCart = cartData.filter(product => item.id !== product.id)
+        handleRestockDeletedItem(item, quantityToAddBackToStock)
+
+        setCartData(filteredCart)
+        handleSetDataLog('subtract', deletedItem[0].product)
+// setChangeDataLog(`${deletedItem[0].product} was removed from the cart`)
+    }
+
+    const handleRestockDeletedItem = (item, quantityToAddBackToStock) => {
+        let newUpdatedData = data.result.map(product => {
+            if (product.id !== item.id) {
+                return product
+            } else {
+                return {...product, quantity: product.quantity + quantityToAddBackToStock}
+            }
+        })
+        setData({...data, result: newUpdatedData})
+    }
+
+    const handleCheckout = async () => {
+        const checkoutData = {
+            requestId: new Date().getTime().toString(16),
+            items: cartData
+        }
+        setIsCheckoutLoading(true)
+
+        try {
+            const request = await fetch(`${REACT_APP_API_URL}/v1/order/create`, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify(checkoutData)
+                // body: JSON.stringify(checkoutData)
+            })
+            const response = await request.json()
+            if (response && response.status === "SUCCESS") {
+                toast.success("Checkout is Successful", {
+                    position: toast.POSITION.TOP_CENTER
+                });
+                setCartData([])
+                setIsCheckoutLoading(false)
+            } else {
+                console.log(response)
+                toast.error(response.description || response.message, {
+                    position: toast.POSITION.TOP_CENTER
+                });
+                setIsCheckoutLoading(false)
+            }
+        } catch (error) {
+            toast.error(error.message, {
+                position: toast.POSITION.TOP_CENTER
+            });
+            setIsCheckoutLoading(false)
+        }
     }
 
     const handleAuthentication = (val) => {
@@ -282,11 +285,11 @@ if(response && response.status === "SUCCESS"){
 
     return (
         <StateContext.Provider
-          value={value}
+            value={value}
         >
-          {props.children}
+            {props.children}
         </StateContext.Provider>
-      );
+    );
 }
 
 export default Context;
